f1dea1ddb369edd8a43f0c916104690a696a0956
ShauryaKarwa <karwashaurya@gmail.com>
2026-07-26 12:27:41 +0530

    Drawing the board and adding some UI features

--- game.py
+++ game.py
@@ -5,7 +5,8 @@ from board import Board
 from Pieces import bishop, king, knight, pawn, piece, queen, rook
 
 class Game:
-    def __init__(self):
+    def __init__(self, screen):
+        self.screen = screen
         self.board = Board()
         self.current_turn = "w"
         self.selected_piece = None
@@ -44,7 +45,27 @@ class Game:
         pass
 
     def draw(self):
-        pass
+        colour = (255, 255, 255)
+        self.screen.fill(colour)
+
+        self.board.draw(self.screen)
+
+        if self.selected_piece is not None:
+            row, col = self.selected_piece.pos
+            highlight = (255, 139, 39)
+            pygame.draw.rect(self.screen, highlight, [120 + self.board.width*col, 20 + self.board.height*row, self.board.width, self.board.height], 3)
+
+            legal_moves = self.selected_piece.get_legal_moves(self.board)
+            for move in legal_moves:
+                row_move, col_move = move
+                pygame.draw.circle(self.screen, (255, 0, 0),  [155 + self.board.width*col_move, 55 + self.board.height*row_move, self.board.width, self.board.height], 20, 2)
+
+        pygame.display.flip()
+
+
 
     def switch_turn(self):
-        pass
+        if self.current_turn == "w":
+            self.current_turn = "b"
+        else:
+            self.current_turn = "w"
